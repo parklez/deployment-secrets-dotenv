3b81a9e668b71cb5d6288a7bcba7e8fd5cec63e4
Andreas <>
2024-04-15 23:23:16 -0300

    Update dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="secret_fetcher",  # Required
-    version="0.2.3",  # Required
+    version="0.2.4",  # Required
     description="Fetch deployment variables from kubernetes and write local .env file.",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
@@ -21,6 +21,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3 :: Only",
     ],
     keywords="kubernetes, secrets, dotenv",  # Optional
